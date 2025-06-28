7aab469bcc88c67b0e6c963f1f924b6f7f3b0be5
gabidudda <gabi.duda2503@gmail.com>
2025-06-27 22:50:25 -0300

    aula 5

---
+++ script.js
@@ -0,0 +1,26 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelector('.parametro-senha_botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho() {
+   //tamanhoSenha = tamanhoSenha-1;
+   if (tamanhoSenha > 1) {
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha
+   }
+
+   }
+}
+
+function aumentaTamanho() {
+   if  (tamanhoSenha > 20) {
+    tamanhoSenha++;
+    numeroSenha.textContent = tamanhoSenha;
+   }
+}
